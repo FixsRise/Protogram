ac7eb9e0259df3632227e226d0d171b0985dca72
FixsRise <fixs228@gmail.com>
2025-08-09 15:40:53 +0200

    back to old version

--- main/views.py
+++ main/views.py
@@ -13,20 +13,10 @@ from posts.models import Post
 def index(request):
     post_list = Post.objects.all().order_by('-created_at')
     last_news_id = request.session.get('last_news_id')
-    paginator = Paginator(post_list, 5)  # 5 постов на страницу
-    page_number = request.GET.get('page', 1)
-    page_obj = paginator.get_page(page_number)
 
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        html = render_to_string('posts/post_list_partial.html', {'posts': page_obj.object_list})
-        return JsonResponse({
-            'html': html,
-            'has_next': page_obj.has_next()
-        })
 
     context = {
-        'posts': page_obj.object_list,
-        'has_next': page_obj.has_next(),
+        'posts': post_list,
         'last_news_id': last_news_id,
     }
     return render(request, 'main/index.html', context)
